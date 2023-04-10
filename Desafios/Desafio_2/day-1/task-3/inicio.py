716ed3b016cb32cde7ca9efd9c143393ba6df7d0
MattheusDuarte <matheusduarte.silva@outlook.com>
2023-04-10 13:01:25 -0300

    atualização da tarefa 3

--- Desafios/Desafio_2/day-1/task-3/inicio.py
+++ Desafios/Desafio_2/day-1/task-3/inicio.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import base64
 
 def inicio():
   print("Iniciando a execucao")
@@ -17,8 +18,10 @@ def loop(var1):
 
 def wFile(fname):
   content = 'TWVuc2FnZW0gc2VjcmV0YSwgdm9jZSBjb25zZWd1aXUuIE8gY29kaWdvIMOpIDIwMjIuCg==\n'
+  decoded_str = base64.b64decode(content).decode('utf-8')
+  print(decoded_str)
   f = open(fname, 'w')
-  f.write(content)
+  f.write(decoded_str)
   f.close()
 
 
